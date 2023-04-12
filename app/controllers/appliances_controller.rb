875d5733c80aeef5e2af3f515aad4267d743642a
Miray Karabulut <mimikarabulut@gmail.com>
2023-04-12 13:38:41 -0500

    add index create and update actions

--- app/controllers/appliances_controller.rb
+++ app/controllers/appliances_controller.rb
@@ -1,5 +1,38 @@
 class AppliancesController < ApplicationController
 
+  def index
+    @appliances = Appliance.all
+    render :index
+  end
+  
+  def create
+    @appliance = Appliance.create(
+      name: params[:name],
+      color: params[:color],
+      location: params[:location],
+    )
+    render :show
+  end
+  
+  # test "show" do
+  #   get "/appliances/#{Appliance.first.id}.json"
+  #   assert_response 200
+
+  #   data = JSON.parse(response.body)
+  #   assert_equal ["id", "name", "color", "location", "created_at", "updated_at"], data.keys
+  # end
+
+  def update
+    @appliance = Appliance.find_by(id: params[:id])
+    @appliance.update(
+      name: params[:name] || @appliance.name,
+      color: params[:color] || @appliance.color,
+      location: params[:location] || @appliance.location,
+    )
+    render :show
+  end
 
 
+  
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
-get "/appliances" => "appliances/index"
+get "/appliances" => "appliances#index"
 
+post "/appliances" => "appliances#create"
 
+patch "/appliances/:id" => "appliances#update"
+
+delete "/appliances:/id" => "appliances#destroy"
 end
